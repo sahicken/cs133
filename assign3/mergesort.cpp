c42c058ef74eb51b91130e50205fcb4e9a1667f2
Steven Hicken <shicken@fccsci.fullcoll.edu>
2023-09-26 11:10:10 -0700

    implemented mergeSort()

--- assign3/mergesort.cpp
+++ assign3/mergesort.cpp
@@ -4,23 +4,44 @@
  */
 
 /* merge(input, size, output, asc)
-   Merge the two "sections" of the array input (which has `size` elements) into
+   Merge the two halves of the array input (which has size elements) into
    output. If asc is true, then the output array should be in ascending order;
-   otherwise it should be descending. 
-
-   NOTE: It is allowed for the sections to be of *any* size; i.e., the dividing
-   line between ascending and descending is not always at `size/2`!
-*/
+   otherwise it should be descending.
+   */
 void merge(int* input, int size, int* output, bool output_asc) {
-    // Your merge implementation goes here
+	// Your merge implementation goes here
+	// this is where the comparison operations go
 }
 
 /* mergesort(input, size, output, asc)
    Mergesort the input array (with size elements) into the output array. If
    asc is true, the output array should be sorted ascending, otherwise it should
    be descending.
-*/
-void mergesort(int *input, int size, int* output, bool output_asc) {
-    // Your mergesort implementation goes here
+   */
+void mergesort(int* input, int size, int* output, bool output_asc) {
+	// Your mergesort implementation goes here
+	// base case
+	// recursive case
+	// recursion parameter
+	if (size==0) return; // base case 0
+	else if (size==1) { // base case 1
+		output=input;
+		return;
+	} else {
+		int mid = size/2;
+		mergeSort(input, mid, output, true); // left side asc
+		mergeSort(input[mid], mid, output[mid], false); // right side desc
+	}
+	merge(input, size, output, output_asc);
 }
 
+/* mergesort(input, size)
+   Sorts size elements in the array pointed to by input, using the MergeSort
+   algorithm. Output is returned as a newly allocated array, which the caller
+   is responsible for freeing.
+   */
+int* mergesort(int* input, int size) {
+	int* output = new int[size];
+	mergesort(input, size, output, true);
+	return output;
+}
