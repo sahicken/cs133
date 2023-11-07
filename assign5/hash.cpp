5080f5fb40ff6487ce99be1cc10ca27f27ef7426
Steven Hicken <sahicken@gmail.com>
2023-11-07 11:20:38 -0800

    init hashes commit

---
+++ assign5/hash.cpp
@@ -0,0 +1,14 @@
+// Your input will be strings, in fact,
+// all the strings that are defined in
+// the system’s dictionary. This is
+// located in /usr/share/dict/words
+
+#include <cstdint>
+#include <boost/math/distributions/chi_squared.hpp>
+
+uint16_t x; // x has exactly 16 bits, and is unsigned
+
+int main()
+{
+    return 0;
+}
